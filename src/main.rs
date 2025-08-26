75f35ff0eacb0fda4d438bfb7bfcded535b18074
Stefano Carraro <stefanodpc29@gmail.com>
2025-08-26 18:42:17 -0300

    added handle and schedule funcs for chegada and saida

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+// Vars for RNG
 const A:u128 = 6364136223846793005;
 const C:u128 = 1442695040888963407;
 const M:u128 = u64::MAX as u128;
 
+const REPETICOES:u16 = 1000;
+
 static mut current_seed:u128 = 7;
 
-const REPETICOES:u16 = 1000;
+// Vars for the queue
+const CHEGADA_MIN:u16 = 1;
+const CHEGADA_MAX:u16 = 5;
+const ATENDIMENTO_MIN:u16 = 2;
+const ATENDIMENTO_MAX:u16 = 6;
+
+const SERVIDORES:u16 = 3;
+const CAPACIDADE:u16 = 5;
+
+// Global vars for queue state
+static mut tamanho_atual:u16 = 0;
 
 fn main() {
     for _ in 0..REPETICOES {
@@ -26,4 +39,34 @@ fn linear_congruencial_gen(seed: u128) -> u128 {
 fn next_random() -> f64 {
     unsafe { current_seed = linear_congruencial_gen(current_seed) };
     (unsafe { current_seed } as f64)/(M as f64)
+}
+
+fn handle_chegada() -> bool {
+    unsafe {
+        if tamanho_atual < CAPACIDADE {
+            tamanho_atual += 1;
+            return true;
+        }
+    }
+    false
+}
+
+fn handle_saida() -> bool {
+    unsafe {
+        if tamanho_atual > 0 {
+            tamanho_atual -= 1;
+            return true;
+        }
+    }
+    false
+}
+
+fn schedule_chegada() -> u16 {
+    let random = next_random();
+    (CHEGADA_MIN as f64 + (CHEGADA_MAX - CHEGADA_MIN) as f64 * random) as u16
+}
+
+fn schedule_saida() -> u16 {
+    let random = next_random();
+    (ATENDIMENTO_MIN as f64 + (ATENDIMENTO_MAX - ATENDIMENTO_MIN) as f64 * random) as u16
 }
